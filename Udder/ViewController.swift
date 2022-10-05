2184496890ebbcdbdf5b3f5f5c2fca8bfb5e4630
Suleyman Saparow <suleymansprv@gmail.com>
2022-10-05 08:41:43 +0500

    Add branch

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override class func attemptRecovery(fromError error: Error, optionIndex recoveryOptionIndex: Int) -> Bool {
+        <#code#>
+    }
 
 }
 
